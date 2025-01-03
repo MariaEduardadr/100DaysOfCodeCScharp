b5d50dde9bf08f2439a704ac74fa5e07b78d1de5
MariaEduardadr <mariaeduarda.reis1404@gmail.com>
2025-01-03 20:50:18 -0300

    remove: removendo arquivos que não fazem parte do projeto

---
+++ Day01/ex01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01
+{
+    internal class ex01
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day01/ex02.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01
+{
+    internal class ex02
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadLine();
+        }
+    }
+}
