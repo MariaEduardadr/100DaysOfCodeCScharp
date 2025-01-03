c5a3ecb5cce38a22a5bcc9a07d0d4adf75914e5e
MariaEduardadr <mariaeduarda.reis1404@gmail.com>
2025-01-03 20:59:52 -0300

    feat: primeiros exercícios recursivos

--- Day01/ex01.cs
+++ Day01/ex01.cs
@@ -8,10 +8,15 @@ namespace Day01
 {
     internal class ex01
     {
+        public static int fatorialRecursivo(int n)
+        {
+            if (n == 0) return 1;
+            else return n * fatorialRecursivo(n - 1);
+        }
         static void Main(string[] args)
         {
 
-
+            fatorialRecursivo(5);
 
             
             Console.ReadLine();
--- Day01/ex02.cs
+++ Day01/ex02.cs
@@ -8,9 +8,15 @@ namespace Day01
 {
     internal class ex02
     {
+        public static int FibonacciRecursivo(int n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+            else return FibonacciRecursivo(n - 1) + FibonacciRecursivo(n - 2);
+        }
         static void Main(string[] args)
         {
-
+            FibonacciRecursivo(10);
 
             Console.ReadLine();
         }
